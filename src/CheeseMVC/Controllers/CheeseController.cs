30b18186a1a33f36dc1f46a3d2705cdf949c4b6c
Tom Corcoran <tjc.corcoran@gmail.com>
2017-06-04 20:52:33 -0500

    completed exercise

--- src/CheeseMVC/Controllers/CheeseController.cs
+++ src/CheeseMVC/Controllers/CheeseController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,12 +12,10 @@ namespace CheeseMVC.Controllers
     public class CheeseController : Controller
     {
 
-        static private Dictionary<string, string> Cheeses = new Dictionary<string, string>();
-
         // GET: /<controller>/
         public IActionResult Index()
         {
-            ViewBag.cheeses = Cheeses;
+            ViewBag.cheeses = CheeseData.GetAll();
 
             return View();
         }
@@ -28,12 +27,50 @@ namespace CheeseMVC.Controllers
 
         [HttpPost]
         [Route("/Cheese/Add")]
-        public IActionResult NewCheese(string name, string description = "")
+        public IActionResult NewCheese(Cheese newCheese)
         {
             // Add the new cheese to my existing cheeses
-            Cheeses.Add(name, description);
+            CheeseData.Add(newCheese);
 
             return Redirect("/Cheese");
+        }
+
+        public IActionResult Remove()
+        {
+            ViewBag.title = "Remove Cheese";
+            ViewBag.cheeses = CheeseData.GetAll();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Remove(int [] cheeseIds)
+        {
+            // TODO
+            foreach(int cheeseId in cheeseIds)
+            {
+                CheeseData.Remove(cheeseId);
+            }
+            return Redirect("/");
+        }
+        // Handle requests to /Cheese/Edit/cheeseId (URL segment)
+        [Route("/Cheese/Edit/{cheeseId}")]
+        public IActionResult Edit(int cheeseId)
+        {
+            ViewBag.title = "Edit Cheese";
+            ViewBag.editCheese = CheeseData.GetById(cheeseId);
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Cheese/Edit/{cheeseId}")]
+        public IActionResult Edit(int cheeseId, string name, string description)
+        {
+            // TODO
+            // Delete the existing entry
+            Cheese editCheese = CheeseData.GetById(cheeseId);
+            editCheese.Name = name;
+            editCheese.Description = description;
+            return Redirect("/");
         }
     }
 }
